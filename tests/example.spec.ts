f4f53fb5d38efdd00475b4bc56bd3f684d6965d4
Matheus Douglas <matheusdouglas1001@gmail.com>
2023-06-09 15:36:14 -0300

    Enviando novos artefatos de teste

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,17 +3,30 @@ import { LoginPage } from '../pages/loginPage'
 import { CadastrarPage } from '../pages/cadastrarPage';
 
 
-test('Cadastrar usuario', async ({ page }) => {
+test.describe('Executar dois testes', () => { 
+
+  test('Cadastrar usuario com sucesso', async ({ page }) => {
 
   const loginPage = new LoginPage(page)
   const cadastrarPage = new CadastrarPage(page)
 
-
   await loginPage.goto();
   await loginPage.linkCadastro();
-  await cadastrarPage.fazerCadastro('matheus12', 'matheus12@gmail.com', '123456')
+  await cadastrarPage.fazerCadastro('diego', 'diego@gmail.com', '123456')
   await cadastrarPage.verificaToast('Conta criada com sucesso')
 
+})
+
+  test('Erro ao cadastrar um usuario', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const cadastrarPage = new CadastrarPage(page)
+    
+    await loginPage.goto();
+    await loginPage.linkCadastro();
+    await cadastrarPage.fazerCadastro('matheus12', 'matheus12@gmail.com', '123456')
+    await cadastrarPage.verificaToast('Erro ao cadastrar')
+
+  })
 
 })
 
